d169ebb218f553962951958b12fa20466370cd98
XargonWan <XargonWan@gmail.com>
2025-07-07 14:30:56 +0900

    Add run_actions to execute sequences (#66)

---
+++ plugins/test_action_plugin.py
@@ -0,0 +1,12 @@
+# plugins/test_action_plugin.py
+"""Dummy plugin used for unit tests of action_parser."""
+
+executed_actions = []
+
+class TestActionPlugin:
+    def get_supported_actions(self):
+        # Use built-in 'command' type so validation succeeds
+        return ["command"]
+
+    async def execute_action(self, action, context, bot, original_message):
+        executed_actions.append(action)
